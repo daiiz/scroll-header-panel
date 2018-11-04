9e945ad5777d69bcc4f53747e9f12ee1517bd001
daiiz <da.iz.mg@gmail.com>
2018-11-04 14:03:17 +0900

    type Bできた

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ class ScrollHeaderPanel extends HTMLElement {
   constructor (...props) {
     super(...props)
     this.panelColor = `#312319`
+    this.titleColor = `rgb(234, 223, 198)`
+    this.bodyColor = `#e9e7e4`
+    this.solid = true
+    this.headerHeight = 192
+    this.titleHeight = 64
+    this.iconSrcUrl = `./miilclient.png`
+    this.panelSrcUrl = `./miilclient.bg.jpg`
     this.render()
   }
 
@@ -14,25 +21,25 @@ class ScrollHeaderPanel extends HTMLElement {
           height: 100vh;
         }
         .header {
-          height: 192px;
           background: #eee;
           width: 100%;
           position: fixed;
-          background-image: url(https://i.gyazo.com/0c15b03645d299f47a4a408b3e6e8388.jpg);
           background-size: cover;
           background-position: center center;
+          background-image: url(${this.panelSrcUrl});
+          height: ${this.headerHeight}px;
         }
         .header-icon {
           width: 40px;
           height: 40px;
-          background-color: rgba(0, 0, 0, 0);
           top: 12px;
           left: 18px;
           position: fixed;
-          background-image: url(https://miilclient.herokuapp.com/static/assets/yummy.png);
           background-size: cover;
           z-index: 20;
           cursor: pointer;
+          background-color: rgba(0, 0, 0, 0);
+          background-image: url(${this.iconSrcUrl});
         }
         .header-slot {
           position: fixed;
@@ -44,23 +51,23 @@ class ScrollHeaderPanel extends HTMLElement {
         .header-color-panel {
           position: absolute;
           width: 100%;
-          height: 192px;
           top: 0;
           opacity: 0;
           z-index: 5;
+          height: ${this.headerHeight}px;
           background-color: ${this.panelColor};
         }
         .title {
-          background: transparent;
           position: absolute;
           padding-left: 20px;
           bottom: 0;
           font-size: 40px;
-          color: rgb(234, 223, 198);
-          top: 128px;
-          height: 64px;
           width: 100vw;
           z-index: 10;
+          background: ${this.solid ? this.panelColor : transparent};
+          color: ${this.titleColor};
+          top: ${this.solid ? this.headerHeight : this.headerHeight - this.titleHeight}px;
+          height: ${this.titleHeight}px;
         }
         .t {
           transform: scale(0.75) translateZ(0px);
@@ -72,8 +79,8 @@ class ScrollHeaderPanel extends HTMLElement {
         .body {
           position: absolute;
           width: 100%;
-          top: 192px;
-          background: rgb(233, 231, 228);
+          background: ${this.bodyColor};
+          top: ${this.solid ? this.headerHeight + this.titleHeight : this.headerHeight}px;
           /* height: calc(100% - 192px); */
         }
       </style>
@@ -111,19 +118,24 @@ class ScrollHeaderPanel extends HTMLElement {
 
   applyStyle (dom, css) {
     const keys = Object.keys(css)
-    for (const key of keys) {
-      console.log(dom)
-      dom.style[key] = `${css[key]}`
-    }
+    for (const key of keys) dom.style[key] = `${css[key]}`
   }
 
   onScroll () {
-    const y = window.scrollY
     const titleBar = this.shadowRoot.querySelector('.title')
     const header = this.shadowRoot.querySelector('.header')
-    const headerHeight = header.offsetHeight
     const headerColorPanel = this.shadowRoot.querySelector('.header-color-panel')
 
+    if (!this.solid) {
+      this.behaviorDefault({titleBar, header, headerColorPanel})
+    } else {
+      this.behaviorSolid({titleBar, header, headerColorPanel})
+    }
+  }
+
+  behaviorDefault ({titleBar, header, headerColorPanel}) {
+    const y = window.scrollY
+    const headerHeight = header.offsetHeight
     if (y < headerHeight - titleBar.offsetHeight) {
       this.applyStyle(titleBar, {
         position: 'absolute',
@@ -135,10 +147,26 @@ class ScrollHeaderPanel extends HTMLElement {
       })
     } else {
       this.applyStyle(titleBar, {
-        top: 0,
         position: 'fixed',
+        top: 0,
         opacity: 1,
         backgroundColor: this.panelColor
+      })
+    }
+  }
+
+  behaviorSolid ({titleBar, header, headerColorPanel}) {
+    const y = window.scrollY
+    const headerHeight = header.offsetHeight
+    if (y < headerHeight) {
+      this.applyStyle(titleBar, {
+        position: 'absolute',
+        top: `${headerHeight}px`,
+      })
+    } else {
+      this.applyStyle(titleBar, {
+        position: 'fixed',
+        top: 0
       })
     }
   }
